cf83e65f6c8e3e6e112d2fcf49aae85a3d0599eb
Kirill Gmirya <Gmiryk_Kirill@mail.ru>
2026-02-06 20:53:25 +0300

    new tests

--- tests/test_task_1_1.py
+++ tests/test_task_1_1.py
@@ -5,20 +5,9 @@ params = ((1, 8, 15), (1, -13, 12), (-4, 28, -49))
 
 
 @pytest.mark.parametrize("a,b,c", params)
-def test_solution_(a, b, c):
+def test_solution(a, b, c):
     assert solution(a, b, c)
 
 
-params1 = ((1, 1, 1))
-
-
-@pytest.mark.parametrize("a,b,c", params1)
-def test_diskriminant_пreater_than_or_equal_to(a, b, c):
-    assert solution(a, b, c) is None
-
-
-def test_all_zero_coefficients():
-    """Тест, когда все коэффициенты равны нулю"""
-    result = solution(0, 0, 0)
-    # 0 = 0 - бесконечно много решений
-    assert result is None
+def test_discriminant_less_zero():
+    assert solution(1, 1, 1) is None
